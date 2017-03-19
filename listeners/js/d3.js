09af6eae8c2bd3adf28c853e3efe8bfb68105717
benjaminsw <benjaminsw@live.com>
2017-03-19 10:59:48 +0700

    added mouse click events

--- listeners/js/d3.js
+++ listeners/js/d3.js
@@ -1,8 +1,21 @@
 // JavaScript source code
 d3.selectAll('.hover-me')
-  .on('mouseover', function () {
-      this.style.backgroundColor = 'yellow';
-  })
-  .on('mouseleave', function () {
-      this.style.backgroundColor = '';
-  });
+    .on('mouseover', function () {
+        this.style.backgroundColor = 'yellow';
+    })
+    .on('mouseleave', function () {
+        this.style.backgroundColor = '';
+    });
+
+
+d3.selectAll('click-me')
+    .on('click', () => {
+        if (this.style.backgroundColor) {
+            this.style.backgroundColor = '';
+        } else {
+            this.style.backgroundColor = 'red';
+        }
+
+    })
+
+
